4c397a44ac5990aa92c08ad857ce2df8e087d557
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 21:32:05 +0530

    Updates: SiteNameFeatureFlag.kt
    Updates: SiteNameFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/SiteNameFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/SiteNameFeatureFlag.kt
@@ -3,12 +3,12 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration of the Site Name step in the Site Creation flow
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.SITE_NAME)
 class SiteNameFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(appConfig, BuildConfig.SITE_NAME)
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(appConfig)
