25b9b68e28840e8f8fc06778651edd8297a29901
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 20:53:26 +0530

    Updates: BetaSiteDesignsFeatureFlag to extend from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/BetaSiteDesignsFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/BetaSiteDesignsFeatureFlag.kt
@@ -3,12 +3,12 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration of the Site Name step in the Site Creation flow
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.BETA_SITE_DESIGNS)
 class BetaSiteDesignsFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(appConfig, BuildConfig.BETA_SITE_DESIGNS)
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(appConfig)
