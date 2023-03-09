f58c936aa3278f2964f192bff37e4d1604dab177
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 21:52:28 +0530

    Updates: BlazeFeatureFlag.kt
    Updates: BlazeFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/remote/BlazeFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/remote/BlazeFeatureFlag.kt
@@ -3,16 +3,15 @@ package org.wordpress.android.util.config.featureflags.remote
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.RemoteFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.RemoteFeatureFlag
 import javax.inject.Inject
 
 private const val BLAZE_FEATURE_REMOTE_FIELD = "blaze"
 
-@RemoteFeatureFlagDefault(BLAZE_FEATURE_REMOTE_FIELD, false)
+@RemoteFeatureFlagDefault(BLAZE_FEATURE_REMOTE_FIELD, BuildConfig.ENABLE_BLAZE_FEATURE)
 class BlazeFeatureFlag @Inject constructor(
     appConfig: AppConfig
-) : FeatureFlag(
+) : RemoteFeatureFlag(
     appConfig,
-    BuildConfig.ENABLE_BLAZE_FEATURE,
     BLAZE_FEATURE_REMOTE_FIELD
 )
