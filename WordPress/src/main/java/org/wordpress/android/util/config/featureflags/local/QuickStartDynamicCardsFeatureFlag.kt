4688a59af21f3e0ed72071927fee112b8cb95257
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 21:29:16 +0530

    Updates: QuickStartDynamicCardsFeatureFlag.kt
    Updates: QuickStartDynamicCardsFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/QuickStartDynamicCardsFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/QuickStartDynamicCardsFeatureFlag.kt
@@ -3,17 +3,16 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration of the Quick Start Dynamic cards within the My Site improvements
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.QUICK_START_DYNAMIC_CARDS)
 class QuickStartDynamicCardsFeatureFlag
 @Inject constructor(
     appConfig: AppConfig
-) : FeatureFlag(
-    appConfig,
-    BuildConfig.QUICK_START_DYNAMIC_CARDS
+) : LocalFeatureFlag(
+    appConfig
 )
