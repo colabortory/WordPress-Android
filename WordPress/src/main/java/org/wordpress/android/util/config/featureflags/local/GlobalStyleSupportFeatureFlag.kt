1f0e0eb8c0d3ab3e19ed2fb6d868a3e7f5740948
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 20:55:04 +0530

    Updates: GlobalStyleSupportFeatureFlag.kt
    Updates: GlobalStyleSupportFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/GlobalStyleSupportFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/GlobalStyleSupportFeatureFlag.kt
@@ -3,12 +3,12 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration of the Global Style Support
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.GLOBAL_STYLE_SUPPORT)
 class GlobalStyleSupportFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(appConfig, BuildConfig.GLOBAL_STYLE_SUPPORT)
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(appConfig)
