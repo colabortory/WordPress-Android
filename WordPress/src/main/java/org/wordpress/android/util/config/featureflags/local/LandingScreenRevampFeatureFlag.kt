e535a813df52032cb9757c75eb132a41729cedd0
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 21:27:46 +0530

    Updates: LandingScreenRevampFeatureFlag.kt
    Updates: LandingScreenRevampFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/LandingScreenRevampFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/LandingScreenRevampFeatureFlag.kt
@@ -3,12 +3,12 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration for the landing screen revamp work
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.LANDING_SCREEN_REVAMP)
 class LandingScreenRevampFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(appConfig, BuildConfig.LANDING_SCREEN_REVAMP)
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(appConfig)
