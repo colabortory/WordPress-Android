32079ca4adf7bc924a135bb7236100e842e9fa97
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 21:28:36 +0530

    Updates: LandOnTheEditorFeatureFlag.kt
    Updates: LandOnTheEditorFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/LandOnTheEditorFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/LandOnTheEditorFeatureFlag.kt
@@ -3,15 +3,14 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration for landing on the editor at the end of the Site Creation flow
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.LAND_ON_THE_EDITOR)
 class LandOnTheEditorFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(
-    appConfig,
-    BuildConfig.LAND_ON_THE_EDITOR
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(
+    appConfig
 )
