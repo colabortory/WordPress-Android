1eb57be0c1d481c9b38eb3810e39e408e03bf792
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 21:27:05 +0530

    Updates: JetpackMigrationFlowFeatureFlag.kt
    Updates: JetpackMigrationFlowFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/JetpackMigrationFlowFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/JetpackMigrationFlowFeatureFlag.kt
@@ -3,15 +3,15 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
 /**
  * Configuration of the Jetpack Migration Flow.
  */
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.JETPACK_MIGRATION_FLOW)
 class JetpackMigrationFlowFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(appConfig, BuildConfig.JETPACK_MIGRATION_FLOW) {
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(appConfig) {
     override fun isEnabled(): Boolean {
         return BuildConfig.IS_JETPACK_APP && super.isEnabled()
     }
