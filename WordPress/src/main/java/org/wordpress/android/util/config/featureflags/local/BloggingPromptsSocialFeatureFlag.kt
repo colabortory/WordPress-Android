3242cdad0b0b5d70acde9bf59425f2de9ce8672a
Ajesh R Pai <ajeshr2k1@gmail.com>
2023-03-09 20:54:23 +0530

    Updates: BloggingPromptsSocialFeatureFlag.kt
    Updates: BloggingPromptsSocialFeatureFlag.kt to extend
    from Local Feature Flag

--- WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/BloggingPromptsSocialFeatureFlag.kt
+++ WordPress/src/main/java/org/wordpress/android/util/config/featureflags/local/BloggingPromptsSocialFeatureFlag.kt
@@ -3,14 +3,13 @@ package org.wordpress.android.util.config.featureflags.local
 import org.wordpress.android.BuildConfig
 import org.wordpress.android.annotation.LocalFeatureFlagDefault
 import org.wordpress.android.util.config.AppConfig
-import org.wordpress.android.util.config.FeatureFlag
+import org.wordpress.android.util.config.LocalFeatureFlag
 import javax.inject.Inject
 
-@LocalFeatureFlagDefault
+@LocalFeatureFlagDefault(BuildConfig.BLOGGING_PROMPTS_SOCIAL)
 class BloggingPromptsSocialFeatureFlag
-@Inject constructor(appConfig: AppConfig) : FeatureFlag(
-    appConfig,
-    BuildConfig.BLOGGING_PROMPTS_SOCIAL,
+@Inject constructor(appConfig: AppConfig) : LocalFeatureFlag(
+    appConfig
 ) {
     override fun isEnabled(): Boolean {
         return super.isEnabled() && BuildConfig.IS_JETPACK_APP
