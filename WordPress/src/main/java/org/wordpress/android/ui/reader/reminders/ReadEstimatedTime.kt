97869ddd7ac556c82bcd574180277d96b380f568
Renan Lukas <14964993+RenanLukas@users.noreply.github.com>
2023-11-16 05:49:36 -0300

    Rename ReadAverageTime -> ReadEstimatedTime

--- WordPress/src/main/java/org/wordpress/android/ui/reader/reminders/ReadEstimatedTime.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/reminders/ReadEstimatedTime.kt
@@ -1,6 +1,6 @@
 package org.wordpress.android.ui.reader.reminders
 
-class ReadAverageTime {
+class ReadEstimatedTime {
     fun wordsPerMinute(fullText: String): Int =
         fullText.trim().split("\\s+".toRegex()).size / AVERAGE_WORDS_PER_MINUTE
 }
